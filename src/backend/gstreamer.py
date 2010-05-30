ee32f9df85593e73a0eb2aefe2e3a6957f578f1f
Thanh Ha <thanh.ha@alumni.carleton.ca>
2010-05-30 19:15:15 -0400

    created function in backend to disable/enable video recording

--- src/backend/gstreamer.py
+++ src/backend/gstreamer.py
@@ -584,6 +584,13 @@ class Freeseer_gstreamer(BackendInterface):
         self.core.logger.log.debug(self.audio_source + ' loaded.')
         return True
 
+    def set_video_mode(self, mode):
+        '''
+        Activates video recording when mode = True
+        Disables video recording when mode = False
+        '''
+        self.record_video = mode
+
     def enable_video_feedback(self, window_id):
         '''
         Activate video feedback. Will send video to a preview window.
