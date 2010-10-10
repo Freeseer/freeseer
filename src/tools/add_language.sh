db6a2c04e54d887a035fb2b5e62caf016d038a2f
kaheer <kaheer@kaheer-laptop.(none)>
2010-10-10 13:17:08 -0400

    removed need to build when adding languages

--- src/tools/add_language.sh
+++ src/tools/add_language.sh
@@ -1,20 +1,13 @@
 #! /bin/bash
-cd ..
-echo "Cleaning the Build:..."
-make clean > hide_output
-cd tools;
-
  
  if [ ! -z $1 ]; then
-   echo "Creating Language $1 with prefix $2"
-   ./create_language.py $1 $2
+   echo "Creating Language prefix $1"
+   ./create_language.py $1
  fi
 echo "Creating profile: .."
 ./create_pro.py > freeseer.pro
 echo "Creating .ts files: ..."
 pylupdate4 freeseer.pro
 rm freeseer.pro
-cd ..
-echo "Rebuilding: ... "
-make > hide_output
-rm hide_output
+rm languages.txt
+echo "Language Files Generated"
