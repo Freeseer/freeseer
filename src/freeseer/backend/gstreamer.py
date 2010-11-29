52b74f7c9d8494cbbca92597320eb30bd8ecc556
Andrew Ross <fosslc@gmail.com>
2010-11-29 10:05:53 -0500

    switch back to x-raw-rgb to fix local capture

--- src/freeseer/backend/gstreamer.py
+++ src/freeseer/backend/gstreamer.py
@@ -164,7 +164,7 @@ class Freeseer_gstreamer(BackendInterface):
         video_rate_cap = gst.element_factory_make('capsfilter',
                                                     'video_rate_cap')
         video_rate_cap.set_property('caps',
-                        gst.caps_from_string('video/x-raw-yuv, framerate=10/1'))
+                        gst.caps_from_string('video/x-raw-rgb, framerate=10/1'))
         video_scale = gst.element_factory_make('videoscale', 'video_scale')
         video_scale_cap = gst.element_factory_make('capsfilter',
                                                     'video_scale_cap')
@@ -176,7 +176,7 @@ class Freeseer_gstreamer(BackendInterface):
             self.core.logger.log.debug('Recording will be scaled to %sx%s'
                 % (self.recording_width, self.recording_height))
             video_scale_cap.set_property('caps',
-                gst.caps_from_string('video/x-raw-yuv, width=%s, height=%s'
+                gst.caps_from_string('video/x-raw-rgb, width=%s, height=%s'
                 % (self.recording_width, self.recording_height)))
 
         self.player.add(video_src,
@@ -360,7 +360,7 @@ class Freeseer_gstreamer(BackendInterface):
         icecast_scale = gst.element_factory_make('videoscale', 'icecast_scale')
         icecast_scale_cap = gst.element_factory_make('capsfilter', 'icecast_scale_cap')
         icecast_scale_cap.set_property('caps',
-            gst.caps_from_string('video/x-raw-yuv,width=320,height=240'))
+            gst.caps_from_string('video/x-raw-rgb,width=320,height=240'))
         icecast_encoder = gst.element_factory_make('theoraenc', 'icecast_encoder')
         icecast_mux = gst.element_factory_make('oggmux', 'icecast_mux')
         
