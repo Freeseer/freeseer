b6be2e82ca39d13723cfa9f824f31b438fccfa8c
Fei <f.wang@uleth.ca>
2011-04-04 19:22:18 -0600

    remove useless function

--- src/freeseer/frontend/default/configtool/freeseer_configtool.py
+++ src/freeseer/frontend/default/configtool/freeseer_configtool.py
@@ -29,7 +29,6 @@ from os import listdir;
 from freeseer.framework.qt_area_selector import *
 from freeseer.framework.qt_key_grabber import *
 from freeseer.framework.core import *
-#from configcore import *
 from freeseer_configtool_ui import *
 
 LANGUAGE_DIR = 'freeseer/frontend/default/configtool/languages/'
