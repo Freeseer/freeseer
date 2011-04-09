15dacf1ba934a11a2604532b9badaff98363106a
Thanh Ha <thanh.ha@alumni.carleton.ca>
2011-04-08 22:07:55 -0400

    updated setup.py a bit

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
+#!/usr/bin/env python
+
 from distutils.core import setup
 setup(name='freeseer',
-      version='1.9.7',
+      version='2.0',
       description='video studio in a backpack',
       author='fosslc',
       author_email='fosslc@gmail.com',
       url='http://wiki.github.com/fosslc/freeseer/',
       license='GPLv3',
-      data_files=[('/etc/modprobe.d', ['src/modprobe.d/vga2usb.conf']),],
-      packages=['freeseer'],
-      package_dir={'freeseer': 'src'},
-      package_data={'freeseer': ['freeseer']},
+      packages=['freeseer', 'freeseer.backend', 'freeseer.framework', 'freeseer.frontend', 'freeseer.frontend.default'],
+      package_dir={'freeseer': 'src/freeseer'},
       )
