e1f02ebaa79a07e3ee006eeae252fa87e6dbe44d
Thanh Ha <thanh.ha@alumni.carleton.ca>
2011-06-25 22:19:17 -0400

    - Plugin manager can now activate and deactivate plugins - Plugin manager can now save plugin state to plugin.conf

--- src/freeseer/framework/plugin.py
+++ src/freeseer/framework/plugin.py
@@ -1,15 +1,20 @@
 import ConfigParser
+import os
 
 from yapsy.PluginManager import PluginManagerSingleton
 from yapsy.ConfigurablePluginManager import ConfigurablePluginManager
 from yapsy.IPlugin import IPlugin
 
 class PluginManager:
-    def __init__(self):
+    def __init__(self, configdir):
         self.plugman = PluginManagerSingleton().get()
         
-        config = ConfigParser.ConfigParser()
-        self.plugmanc = ConfigurablePluginManager(config, self.plugman)
+        self.configdir = configdir
+        self.configfile = os.path.abspath("%s/plugin.conf" % self.configdir)
+        
+        self.config = ConfigParser.ConfigParser()
+        self.load()
+        self.plugmanc = ConfigurablePluginManager(self.config, self, self.plugman)
         
         self.plugmanc.setPluginPlaces(["freeseer/plugins"])
         self.plugmanc.setCategoriesFilter({
@@ -20,12 +25,35 @@ class PluginManager:
             "Output" : IOutput,
             })
         self.plugmanc.collectPlugins()
-        self.plugmanc.activatePluginByName("Video Test Source", "VideoInput", False)
-        self.plugmanc.activatePluginByName("Video Preview", "Output", False)
-        self.plugmanc.activatePluginByName("Audio Feedback", "Output", False)
+#        self.plugmanc.activatePluginByName("Video Test Source", "VideoInput", False)
+#        self.plugmanc.activatePluginByName("Video Preview", "Output", False)
+#        self.plugmanc.activatePluginByName("Audio Feedback", "Output", False)
+        
+    def __call__(self):
+        pass
+    
+    def load(self):
+        try:
+            self.config.readfp(open(self.configfile))
+        # Config file does not exist, create a default
+        except IOError:
+            self.save()
+            return
+        
+    def save(self):
+        with open(self.configfile, 'w') as configfile:
+            self.config.write(configfile)
+        
+    def activate_plugin(self, plugin_name, plugin_category):
+        self.plugmanc.activatePluginByName(plugin_name, plugin_category, True)
+        self.save()
+        print 'here'
+        
+    def deactivate_plugin(self, plugin_name, plugin_category):
+        self.plugmanc.deactivatePluginByName(plugin_name, plugin_category, True)
+        self.save()
         
     def get_output_plugins(self):
-        
         return None
 
 class IAudioInput(IPlugin):
