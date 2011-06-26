d0b0d919b7878cd0e330edb1905be119328843c4
Thanh Ha <thanh.ha@alumni.carleton.ca>
2011-06-26 19:05:57 -0400

    usbsrc plugin now loads v4l2src.

--- src/freeseer/plugins/usbsrc.py
+++ src/freeseer/plugins/usbsrc.py
@@ -1,10 +1,21 @@
+import pygst
+pygst.require("0.10")
+import gst
+
 from freeseer.framework.plugin import IVideoInput
 
 class USBSrc(IVideoInput):
     name = "USB Source"
     
-    def get_name(self):
-        return self.name
-    
-    def get_source(self):
-        return "usb test" 
+    def get_videoinput_bin(self):
+        bin = gst.Bin(self.name)
+        
+        videosrc = gst.element_factory_make("v4l2src", "videosrc")
+        bin.add(videosrc)
+        
+        # Setup ghost pad
+        pad = videosrc.get_pad("src")
+        ghostpad = gst.GhostPad("videosrc", pad)
+        bin.add_pad(ghostpad)
+        
+        return bin
