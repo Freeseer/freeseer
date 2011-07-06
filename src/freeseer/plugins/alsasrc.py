00d1ea0b8183011a4f461b88cec082e95be63c4b
Thanh Ha <thanh.ha@alumni.carleton.ca>
2011-07-05 20:54:33 -0400

    Added ALSA input plugin.

---
+++ src/freeseer/plugins/alsasrc.py
@@ -0,0 +1,21 @@
+import pygst
+pygst.require("0.10")
+import gst
+
+from freeseer.framework.plugin import IVideoInput
+
+class USBSrc(IVideoInput):
+    name = "ALSA Source"
+    
+    def get_videoinput_bin(self):
+        bin = gst.Bin(self.name)
+        
+        audiosrc = gst.element_factory_make("alsasrc", "audiosrc")
+        bin.add(audiosrc)
+        
+        # Setup ghost pad
+        pad = audiosrc.get_pad("src")
+        ghostpad = gst.GhostPad("audiosrc", pad)
+        bin.add_pad(ghostpad)
+        
+        return bin
