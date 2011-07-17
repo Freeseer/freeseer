085a3e62dd05337dc7640fb9ae87fd6476c5d6b3
Thanh Ha <thanh.ha@alumni.carleton.ca>
2011-07-16 21:06:49 -0400

    Added Picture-In-Picture and necessary Interface updates to support it.

--- src/freeseer/framework/core.py
+++ src/freeseer/framework/core.py
@@ -456,6 +456,7 @@ class FreeseerCore:
         
         if videomixer is not None:
             videoinputs = self.plugman.plugmanc.getPluginsOfCategory("VideoInput")
+            videomixer.load_config(self.plugman)
             self.backend.load_videomixer(videomixer, videoinputs)
         
         self.backend.record()
