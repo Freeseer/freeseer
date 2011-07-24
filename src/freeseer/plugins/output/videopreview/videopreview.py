a3625b1f0e64e300a921d351a5bd84c465925d3a
Thanh Ha <thanh.ha@alumni.carleton.ca>
2011-07-24 18:33:57 -0400

    -Readded metadata support to core. -Added metadata via vorbistag to webm-output.

--- src/freeseer/plugins/output/videopreview/videopreview.py
+++ src/freeseer/plugins/output/videopreview/videopreview.py
@@ -8,7 +8,7 @@ class VideoPreview(IOutput):
     name = "Video Preview"
     type = "video"
     
-    def get_output_bin(self):
+    def get_output_bin(self, metadata=None):
         bin = gst.Bin(self.name)
         
         videoqueue = gst.element_factory_make("queue", "videoqueue")
