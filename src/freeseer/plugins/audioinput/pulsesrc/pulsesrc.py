d3ce32e67b912fcb7b1ac644bc991f5d9a82f13f
Thanh Ha <thanh.ha@alumni.carleton.ca>
2011-07-24 21:16:47 -0400

    Added pulse audio source plugin.

---
+++ src/freeseer/plugins/audioinput/pulsesrc/pulsesrc.py
@@ -0,0 +1,21 @@
+import pygst
+pygst.require("0.10")
+import gst
+
+from freeseer.framework.plugin import IAudioInput
+
+class ALSASrc(IAudioInput):
+    name = "Pulse Audio Source"
+    
+    def get_audioinput_bin(self):
+        bin = gst.Bin(self.name)
+        
+        audiosrc = gst.element_factory_make("pulsesrc", "audiosrc")
+        bin.add(audiosrc)
+        
+        # Setup ghost pad
+        pad = audiosrc.get_pad("src")
+        ghostpad = gst.GhostPad("audiosrc", pad)
+        bin.add_pad(ghostpad)
+        
+        return bin
