ebfaa959ab48217638f3a70cd1ab104026af8dc9
Thanh Ha <thanh.ha@alumni.carleton.ca>
2011-08-07 11:44:06 -0400

    - Updated naming for audio / video passthrough plugins. - On first run freeseer now sets some default plugins. - - ALSA Source - - USB Source - - Audio Passthrough - - Video Passthrough - - Ogg Output

--- src/freeseer/plugins/videomixer/videopassthrough/videopassthrough.py
+++ src/freeseer/plugins/videomixer/videopassthrough/videopassthrough.py
@@ -9,7 +9,7 @@ from PyQt4 import QtGui, QtCore
 from freeseer.framework.plugin import IVideoMixer
 
 class InputSelector(IVideoMixer):
-    name = "VideoPassthrough"
+    name = "Video Passthrough"
     input1 = None
     widget = None
     
