acfa5512d85a9699870835739ae8f2dfd965b5b0
Thanh Ha <thanh.ha@alumni.carleton.ca>
2011-10-08 17:25:07 -0400

    ConfigTool plugin configuration screens have been improved.

--- src/freeseer/frontend/configtool/PluginLoaderWidget.py
+++ src/freeseer/frontend/configtool/PluginLoaderWidget.py
@@ -45,19 +45,45 @@ class PluginLoaderWidget(QtGui.QWidget):
         self.listWidget = QtGui.QListWidget()
         self.mainLayout.addWidget(self.listWidget)
         
-    def getListWidgetPlugin(self, name):
+    def getListWidgetPlugin(self, plugin, plugin_category, plugman):
+        plugin_name = plugin.plugin_object.get_name()
+        
         widget = QtGui.QWidget()
         layout = QtGui.QHBoxLayout()
         widget.setLayout(layout)
         
-        pluginLabel = QtGui.QLabel(name)
-        pluginConfigPushButton = QtGui.QPushButton("...")
-        pluginConfigPushButton.setSizePolicy(QtGui.QSizePolicy.Maximum, QtGui.QSizePolicy.Maximum)
+        # Checkbox, set the proper state on load
+        pluginCheckBox = QtGui.QCheckBox()
+        pluginCheckBox.setSizePolicy(QtGui.QSizePolicy.Maximum, QtGui.QSizePolicy.Maximum)
+        
+        if plugin.is_activated:
+            pluginCheckBox.setCheckState(QtCore.Qt.Checked)
+        else:
+            pluginCheckBox.setCheckState(QtCore.Qt.Unchecked)
+        
+        layout.addWidget(pluginCheckBox)
+
+        pluginLabel = QtGui.QLabel(plugin_name)
         layout.addWidget(pluginLabel)
-        layout.addWidget(pluginConfigPushButton)
+        
+        # Signal to activate/deactivate a plugin.
+        def set_plugin_state():
+            if pluginCheckBox.checkState() == 2:
+                plugman.activate_plugin(plugin_name, plugin_category)
+            else:
+                plugman.deactivate_plugin(plugin_name, plugin_category)
+        
+        widget.connect(pluginCheckBox, QtCore.SIGNAL('clicked()'), set_plugin_state)
+        
+        # If plugin supports configuration, show a configuration button.
+        if plugin.plugin_object.get_widget() is not None:
+            pluginConfigPushButton = QtGui.QPushButton("...")
+            pluginConfigPushButton.setSizePolicy(QtGui.QSizePolicy.Maximum, QtGui.QSizePolicy.Maximum)
+            layout.addWidget(pluginConfigPushButton)
+            self.connect(pluginConfigPushButton, QtCore.SIGNAL('clicked()'), plugin.plugin_object.get_dialog)
         
         return widget
-        
+
 if __name__ == "__main__":
     import sys
     app = QtGui.QApplication(sys.argv)
