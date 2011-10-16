77c86741c640bb3b4fe6dd1d246ac1f0071bfc15
Thanh Ha <thanh.ha@alumni.carleton.ca>
2011-10-16 17:32:26 -0400

    Update chinese translation file.
    Signed-off-by: Thanh Ha <thanh.ha@alumni.carleton.ca>

--- src/freeseer/frontend/qtcommon/languages/tr_zh_CN.ts
+++ src/freeseer/frontend/qtcommon/languages/tr_zh_CN.ts
@@ -1,6 +1,5 @@
 <?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS>
-<TS version="2.0" language="zh_CN" sourcelanguage="en_CA">
+<!DOCTYPE TS><TS version="2.0" language="zh_CN" sourcelanguage="en_CA">
 <context>
     <name>AboutDialog</name>
     <message>
