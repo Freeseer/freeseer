98eaf17c3f447aa2c63fd5686d6874778fc08edc
Thanh Ha <thanh.ha@alumni.carleton.ca>
2011-10-16 01:44:03 -0400

    Added default language (English) to be loaded. Updated ConfigTool with translation support. Updated ConfigTool with menubar.
    Signed-off-by: Thanh Ha <thanh.ha@alumni.carleton.ca>

--- src/freeseer/frontend/configtool/ConfigToolWidget.py
+++ src/freeseer/frontend/configtool/ConfigToolWidget.py
@@ -46,10 +46,6 @@ class ConfigToolWidget(QtGui.QWidget):
         '''
         QtGui.QWidget.__init__(self, parent)
         
-        icon = QtGui.QIcon()
-        icon.addPixmap(QtGui.QPixmap(_fromUtf8(":/freeseer/logo.png")), QtGui.QIcon.Normal, QtGui.QIcon.Off)
-        self.setWindowIcon(icon)
-        
         self.mainLayout = QtGui.QHBoxLayout()
         self.setLayout(self.mainLayout)
         
