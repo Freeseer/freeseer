0fb4628a1111fcc5d50e57294c6a870445d90d60
jshyeung <joseph.sh.yeung@gmail.com>
2011-10-17 15:21:42 -0400

    Added Traditional-Chinese

--- src/freeseer/frontend/qtcommon/languages/tr_ja.ts
+++ src/freeseer/frontend/qtcommon/languages/tr_ja.ts
@@ -1,6 +1,5 @@
 <?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS>
-<TS version="2.0" language="ja_JP">
+<!DOCTYPE TS><TS version="2.0" language="ja_JP" sourcelanguage="">
 <context>
     <name>AboutDialog</name>
     <message>
