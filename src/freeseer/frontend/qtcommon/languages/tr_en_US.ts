997394d4678415197fd49721a905d693a86ce5cb
Thanh Ha <thanh.ha@alumni.carleton.ca>
2011-10-18 22:40:49 -0400

    Updated English translation with translation of new string.
    Signed-off-by: Thanh Ha <thanh.ha@alumni.carleton.ca>

--- src/freeseer/frontend/qtcommon/languages/tr_en_US.ts
+++ src/freeseer/frontend/qtcommon/languages/tr_en_US.ts
@@ -1,5 +1,6 @@
 <?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS><TS version="2.0" language="en_US" sourcelanguage="">
+<!DOCTYPE TS>
+<TS version="2.0" language="en_US">
 <context>
     <name>AboutDialog</name>
     <message>
@@ -188,7 +189,7 @@
     <message>
         <location filename="../../configtool/configtool.py" line="250"/>
         <source>Default Language</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Default Language</translation>
     </message>
 </context>
 <context>
