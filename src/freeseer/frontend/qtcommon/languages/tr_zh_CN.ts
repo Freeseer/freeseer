a5116fe576d3d52e26e46b70100c876ee2001b34
Thanh Ha <thanh.ha@alumni.carleton.ca>
2011-10-19 21:44:04 -0400

    Added translation for Default Language text in japanese and chinese translation files.
    Signed-off-by: Thanh Ha <thanh.ha@alumni.carleton.ca>

--- src/freeseer/frontend/qtcommon/languages/tr_zh_CN.ts
+++ src/freeseer/frontend/qtcommon/languages/tr_zh_CN.ts
@@ -1,5 +1,6 @@
 <?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS><TS version="2.0" language="zh_CN" sourcelanguage="en_CA">
+<!DOCTYPE TS>
+<TS version="2.0" language="zh_CN" sourcelanguage="en_CA">
 <context>
     <name>AboutDialog</name>
     <message>
@@ -188,7 +189,7 @@
     <message>
         <location filename="../../configtool/configtool.py" line="250"/>
         <source>Default Language</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">默认语言</translation>
     </message>
 </context>
 <context>
