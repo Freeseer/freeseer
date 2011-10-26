01952844c3d3591cdd77b0788185ff0ffff23c1c
Thanh Ha <thanh.ha@alumni.carleton.ca>
2011-10-25 21:30:14 -0400

    Updated adder plugin to use new code standard.
    Signed-off-by: Thanh Ha <thanh.ha@alumni.carleton.ca>

--- src/freeseer/plugins/audiomixer/adder/adder.py
+++ src/freeseer/plugins/audiomixer/adder/adder.py
@@ -54,18 +54,16 @@ class Adder(IAudioMixer):
         bin.add_pad(src_ghostpad)
         
         return bin
+    
+    def get_inputs(self):
+        inputs = [self.input1]
+        return inputs
         
     def load_inputs(self, player, mixer, inputs):
-        loaded = []
-        for plugin in inputs:
-            if plugin.is_activated and plugin.plugin_object.get_name() == self.input1:
-                input = plugin.plugin_object.get_audioinput_bin()
-                player.add(input)
-                input.link(mixer)
-                loaded.append(input)
-                break
-            
-        return loaded
+        # load inputs
+        input = inputs[0]
+        player.add(input)
+        input.link(mixer)
 
 
     def load_config(self, plugman):
