8aabf2f1f14eb035ce98a6bf48c7475d104f1a83
Jordan Klassen <forivall@gmail.com>
2011-11-06 22:23:28 -0800

    Added contacting mathieu to todo list tweaking gui added a minimalistic core for access to logging and config

---
+++ src/freeseer/frontend/videouploader/MinimalistCore.py
@@ -0,0 +1,21 @@
+'''
+Created on Nov 6, 2011
+
+@author: jord
+'''
+import logging
+import os
+
+#from PyQt4 import QtGui, QtCore
+#from freeseer.framework.core import FreeseerCore
+from freeseer.framework.config import Config
+from freeseer.framework.logger import Logger
+
+# mainly for testing, to reduce startup time. I'm only using config and logger.
+class MinimalistCore(object):
+    def __init__(self, window=None, audio_feedback=None):
+        # Read in config information
+        configdir = os.path.abspath(os.path.expanduser('~/.freeseer/'))
+        self.config = Config(configdir)
+        self.logger = Logger(configdir)
+        logging.info(u"Mock Core initialized")   
