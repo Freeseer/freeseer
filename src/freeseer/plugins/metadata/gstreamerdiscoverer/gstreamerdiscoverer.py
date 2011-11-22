b6d05b5365d0108eeadc46c63b256e75ae27898f
Jordan Klassen <forivall@gmail.com>
2011-11-21 23:29:20 -0800

    plugins for metadata retrieval implemented need to actually use them now

---
+++ src/freeseer/plugins/metadata/gstreamerdiscoverer/gstreamerdiscoverer.py
@@ -0,0 +1,77 @@
+'''
+freeseer - vga/presentation capture software
+
+Copyright (C) 2011  Free and Open Source Software Learning Centre
+http://fosslc.org
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+For support, questions, suggestions or any other inquiries, visit:
+http://wiki.github.com/fosslc/freeseer/
+
+@author: Jordan Klassen
+'''
+
+
+import functools
+#import pygst
+#pygst.require('0.10')
+#import gst
+from PyQt4 import QtGui, QtCore
+# i don't know if this works with translate's scanner, 
+#  may need some hand coding of translation files
+tr = functools.partial(QtCore.QCoreApplication.translate, "metadata_gstreamerdiscoverer")
+
+from freeseer.framework import uploader
+from freeseer.framework.plugin import IMetadataReader
+
+class GStreamerDiscoverer(IMetadataReader):
+    fields_provided = {
+        'album':            IMetadataReader.header(tr("Album"), str), 
+        'comment':          IMetadataReader.header(tr("Description"), str), 
+#        'performer':        IMetadataReader.header(tr("Performer"), str),
+#        'encoder-version':  IMetadataReader.header(tr("Encoder Version"), int), 
+        'title':            IMetadataReader.header(tr("Title"), str), 
+#        'audio-codec':      IMetadataReader.header(tr("Audio Codec"), str), 
+        'artist':           IMetadataReader.header(tr("Artist"), str), 
+#        'encoder':          IMetadataReader.header(tr("Encoder"), str), 
+#        'nominal-bitrate':  IMetadataReader.header(tr("Nominal Bitrate"), int), 
+#        'date':             IMetadataReader.header(tr("Recording Date"), gst.Date),
+#        'video-codec':      IMetadataReader.header(tr("Video Codec"), str), 
+#        'bitrate':          IMetadataReader.header(tr("Bitrate"), int), 
+#        'container-format': IMetadataReader.header(tr("Format"), str),
+        'duration':         IMetadataReader.header(tr("Duration"), long),
+        'videowidth':       IMetadataReader.header(tr("Width"), int),
+        'videoheight':      IMetadataReader.header(tr("Height"), int),
+        }
+    
+    def retrieve_metadata_internal(self, filepath):
+        # TODO: implement batch begin/end for this class so that the 
+        #  gobject main loop isn't constantly being started and stopped
+        # TODO: call discoverer directly instead of throught uploader.VideoData
+        
+        d = uploader.VideoData(filepath)
+        d.run()
+        
+        return {
+        'album':            d.album, 
+        'comment':          d.comment, 
+        'title':            d.title, 
+        'artist':           d.artist, 
+#        'date':             ,
+        'duration':         d.raw_duration,
+        'videowidth':       d.videowidth,
+        'videoheight':      d.videoheight,
+        }
+        
