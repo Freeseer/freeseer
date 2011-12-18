ef0f7cee54c83c58b2700181fd81ef8db9e6b206
Jordan Klassen <forivall@gmail.com>
2011-12-18 15:38:19 -0800

    Fixed error when starting without a previous upload history Added uncommited changes to TODO file

--- src/freeseer/framework/config.py
+++ src/freeseer/framework/config.py
@@ -198,7 +198,7 @@ class UploaderServerHistoryConfig(BaseSectionConfig):
     defaults = {'username':'',
                 'server':'',
                 'port':str(const.SFTP_DEFAULT_PORT),
-                'servertype':const.NotSelected}
+                'servertype':str(const.NotSelected)}
     
     username = property(*propertyargs('username'))
     server = property(*propertyargs('server'))
