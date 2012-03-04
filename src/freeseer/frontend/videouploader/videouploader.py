166b7e6b79e59dece9a605b8f6a3f4e38d44964c
Thanh Ha <thanh.ha@alumni.carleton.ca>
2012-03-03 22:45:30 -0500

    Update URL links.

--- src/freeseer/frontend/videouploader/videouploader.py
+++ src/freeseer/frontend/videouploader/videouploader.py
@@ -21,7 +21,7 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 For support, questions, suggestions or any other inquiries, visit:
-http://wiki.github.com/fosslc/freeseer/
+http://wiki.github.com/Freeseer/freeseer/
 
 @author: Jordan Klassen
 '''
