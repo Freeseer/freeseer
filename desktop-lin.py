ea96d82325a4ad5a3897110e77bbb84d58a8607e
Thanh Ha <thanh.ha@alumni.carleton.ca>
2012-03-04 21:32:06 -0500

    Moved Input_Type, Framerate, and Resolution options from input plugins to videopassthrough mixer. GH-166

--- desktop-lin.py
+++ desktop-lin.py
@@ -36,9 +36,6 @@ from freeseer.framework.plugin import IVideoInput
 
 class DesktopLinuxSrc(IVideoInput):
     name = "Desktop-Linux Source"
-    input_type = "video/x-raw-rgb"
-    framerate = 10
-    resolution = "NOSCALE"
     
     def get_videoinput_bin(self):
         """
@@ -49,118 +46,10 @@ class DesktopLinuxSrc(IVideoInput):
         videosrc = gst.element_factory_make("ximagesrc", "videosrc")
         bin.add(videosrc)
         
-        
-        # Video Rate
-        videorate = gst.element_factory_make("videorate", "videorate")
-        bin.add(videorate)
-        videorate_cap = gst.element_factory_make("capsfilter",
-                                                    "video_rate_cap")
-        videorate_cap.set_property("caps",
-                        gst.caps_from_string("%s, framerate=%d/1" % (self.input_type, self.framerate)))
-        bin.add(videorate_cap)
-        # --- End Video Rate
-        
-        
-        # Video Scaler (Resolution)
-        videoscale = gst.element_factory_make("videoscale", "videoscale")
-        bin.add(videoscale)
-        videoscale_cap = gst.element_factory_make("capsfilter",
-                                                    "videoscale_cap")
-        if self.resolution != "NOSCALE":
-            videoscale_cap.set_property('caps',
-                                        gst.caps_from_string('%s, width=640, height=480' % (self.input_type)))
-        bin.add(videoscale_cap)
-        # --- End Video Scaler
-        
-        
         # Setup ghost pad
         pad = videoscale_cap.get_pad("src")
         ghostpad = gst.GhostPad("videosrc", pad)
         bin.add_pad(ghostpad)
         
-        gst.element_link_many(videosrc, videorate, videorate_cap, videoscale, videoscale_cap)
-        
         return bin
-    
-    def load_config(self, plugman):
-        self.plugman = plugman
-        self.input_type = self.plugman.plugmanc.readOptionFromPlugin("VideoInput", self.name, "Input Type")
-        self.framerate = self.plugman.plugmanc.readOptionFromPlugin("VideoInput", self.name, "Framerate")
-        self.resolution = self.plugman.plugmanc.readOptionFromPlugin("VideoInput", self.name, "Resolution")
-        
-    def get_widget(self):
-        if self.widget is None:
-            self.widget = QtGui.QWidget()
-            
-            layout = QtGui.QFormLayout()
-            self.widget.setLayout(layout)
-            
-            self.videocolourLabel = QtGui.QLabel(self.widget.tr("Colour Format"))
-            self.videocolourComboBox = QtGui.QComboBox()
-            self.videocolourComboBox.addItem("video/x-raw-rgb")
-            self.videocolourComboBox.addItem("video/x-raw-yuv")
-            self.videocolourComboBox.setSizePolicy(QtGui.QSizePolicy.Minimum, QtGui.QSizePolicy.Maximum)
-            layout.addRow(self.videocolourLabel, self.videocolourComboBox)
-            
-            self.framerateLabel = QtGui.QLabel("Framerate")
-            self.framerateLayout = QtGui.QHBoxLayout()
-            self.framerateSlider = QtGui.QSlider()
-            self.framerateSlider.setOrientation(QtCore.Qt.Horizontal)
-            self.framerateSlider.setMinimum(0)
-            self.framerateSlider.setMaximum(60)
-            self.framerateSpinBox = QtGui.QSpinBox()
-            self.framerateSpinBox.setMinimum(0)
-            self.framerateSpinBox.setMaximum(60)
-            self.framerateLayout.addWidget(self.framerateSlider)
-            self.framerateLayout.addWidget(self.framerateSpinBox)
-            layout.addRow(self.framerateLabel, self.framerateLayout)
-            
-            self.videoscaleLabel = QtGui.QLabel("Video Scale")
-            self.videoscaleComboBox = QtGui.QComboBox()
-            self.videoscaleComboBox.addItem("NOSCALE")
-            self.videoscaleComboBox.setSizePolicy(QtGui.QSizePolicy.Minimum, QtGui.QSizePolicy.Maximum)
-            layout.addRow(self.videoscaleLabel, self.videoscaleComboBox)
-            
-            # Connections
-            self.widget.connect(self.framerateSlider,
-                                QtCore.SIGNAL("valueChanged(int)"),
-                                self.framerateSpinBox.setValue)
-            self.widget.connect(self.framerateSpinBox,
-                                QtCore.SIGNAL("valueChanged(int)"),
-                                self.framerateSlider.setValue)
-            self.widget.connect(self.videocolourComboBox,
-                                QtCore.SIGNAL("currentIndexChanged(const QString&)"),
-                                self.set_videocolour)
-            self.widget.connect(self.framerateSlider,
-                                QtCore.SIGNAL("valueChanged(int)"),
-                                self.set_framerate)
-            self.widget.connect(self.framerateSpinBox,
-                                QtCore.SIGNAL("valueChanged(int)"),
-                                self.set_framerate)
-            
-        return self.widget
 
-    def widget_load_config(self, plugman):
-        self.plugman = plugman
-        
-        try:
-            self.input_type = self.plugman.plugmanc.readOptionFromPlugin("VideoInput", self.name, "Input Type")
-            self.framerate = int(self.plugman.plugmanc.readOptionFromPlugin("VideoInput", self.name, "Framerate"))
-            self.resolution = self.plugman.plugmanc.readOptionFromPlugin("VideoInput", self.name, "Resolution")
-        except ConfigParser.NoSectionError:
-            self.plugman.plugmanc.registerOptionFromPlugin("VideoInput", self.name, "Input Type", self.input_type)
-            self.plugman.plugmanc.registerOptionFromPlugin("VideoInput", self.name, "Framerate", self.framerate)
-            self.plugman.plugmanc.registerOptionFromPlugin("VideoInput", self.name, "Resolution", self.resolution)
-        
-        vcolour_index = self.videocolourComboBox.findText(self.input_type)
-        self.videocolourComboBox.setCurrentIndex(vcolour_index)
-        
-        self.framerateSlider.setValue(self.framerate)
-        
-    def set_videocolour(self, input_type):
-        self.plugman.plugmanc.registerOptionFromPlugin("VideoInput", self.name, "Input Type", input_type)
-        self.plugman.save()
-        
-    def set_framerate(self, framerate):
-        self.plugman.plugmanc.registerOptionFromPlugin("VideoInput", self.name, "Framerate", framerate)
-        self.plugman.save()
