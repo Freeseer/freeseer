074ea4c6c1ff1529b9a5b719263e515398b94fea
Thanh Ha <thanh.ha@alumni.carleton.ca>
2012-03-24 23:27:08 -0400

    Move ReportWidget logic to record.py logic file.

--- src/freeseer/frontend/record/ReportDialog.py
+++ src/freeseer/frontend/record/ReportDialog.py
@@ -34,7 +34,7 @@ import logging
 from freeseer import project_info
 from freeseer.frontend.qtcommon.Resource import resource_rc
 from freeseer.framework.core import FreeseerCore
-from freeseer.framework.failure import *
+
 __version__= project_info.VERSION
 
 
@@ -43,19 +43,13 @@ class ReportDialog(QtGui.QWidget):
     Failure report Dialog for the Freeseer Project. 
 
     """
-    def __init__(self, presentation, talk_ID, core, parent=None):
+    def __init__(self, parent=None):
         QtGui.QWidget.__init__(self, parent)
-        
-        self.current_language = "tr_en_US.qm"
-        self.uiTranslator = QtCore.QTranslator()
-        self.uiTranslator.load(":/languages/tr_en_US.qm")
         
         icon = QtGui.QIcon()
         icon.addPixmap(QtGui.QPixmap(_fromUtf8(":/freeseer/logo.png")), QtGui.QIcon.Normal, QtGui.QIcon.Off)
         self.setWindowIcon(icon)
         
-        self.core = core
-        self.talk_ID = talk_ID
         self.mainWidget = QtGui.QWidget()
         self.mainLayout = QtGui.QVBoxLayout()
         self.setLayout(self.mainLayout)
@@ -68,11 +62,11 @@ class ReportDialog(QtGui.QWidget):
         self.mainLayout.addLayout(self.buttonLayout)
         
         # Talk infomation
-        self.titleLabel = QtGui.QLabel("Title: %s" % presentation.title)
-        self.speakerLabel = QtGui.QLabel("Speaker: %s" % presentation.speaker)
-        self.eventLabel = QtGui.QLabel("Event: %s" % presentation.event)
-        self.roomLabel = QtGui.QLabel("Room: %s" % presentation.room)
-        self.timeLabel = QtGui.QLabel("Time: %s" % presentation.time)
+        self.titleLabel = QtGui.QLabel("Title:")
+        self.speakerLabel = QtGui.QLabel("Speaker:")
+        self.eventLabel = QtGui.QLabel("Event:")
+        self.roomLabel = QtGui.QLabel("Room:")
+        self.timeLabel = QtGui.QLabel("Time:")
         self.infoLayout.addWidget(self.titleLabel)
         self.infoLayout.addWidget(self.speakerLabel)
         self.infoLayout.addWidget(self.eventLabel)
@@ -99,21 +93,3 @@ class ReportDialog(QtGui.QWidget):
         self.buttonLayout.addWidget(self.closeButton)
         self.buttonLayout.addWidget(self.reportButton)
         self.connect(self.closeButton, QtCore.SIGNAL("clicked()"), self.close)
-        self.connect(self.reportButton, QtCore.SIGNAL("clicked()"), self.report)
-        #self.retranslate()
-    
-    def report(self):
-        i = self.reportCombo.currentIndex()
-        print self.talk_ID
-        print self.commentEdit.text()
-        print self.options[i]
-        failure = Failure(self.talk_ID, self.commentEdit.text(), self.options[i])
-        logging.info("Report failure %s %s %s" % (self.talk_ID, self.commentEdit.text(), self.options[i]))
-        self.core.db.insert_failure(failure)
-        self.close()
-   
-    def retranslate(self, language=None):
-        if language is not None:
-            self.current_language = language
-        
-        self.uiTranslator.load(":/languages/tr_%s.qm" % self.current_language)
