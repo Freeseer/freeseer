847492bceb5b8853250b427cae398ad4eddf2588
Thanh Ha <thanh.ha@alumni.carleton.ca>
2012-06-15 22:28:26 -0400

    Improved Video Test Src to support pattern selection and live mode
    Closes #180

--- src/freeseer/plugins/videoinput/videotestsrc/videotestsrc.py
+++ src/freeseer/plugins/videoinput/videotestsrc/videotestsrc.py
@@ -1,7 +1,7 @@
 '''
 freeseer - vga/presentation capture software
 
-Copyright (C) 2011  Free and Open Source Software Learning Centre
+Copyright (C) 2011-2012  Free and Open Source Software Learning Centre
 http://fosslc.org
 
 This program is free software: you can redistribute it and/or modify
@@ -23,19 +23,34 @@ http://wiki.github.com/Freeseer/freeseer/
 @author: Thanh Ha
 '''
 
+import ConfigParser
+
 import pygst
 pygst.require("0.10")
 import gst
+
+from PyQt4 import QtGui, QtCore
 
 from freeseer.framework.plugin import IVideoInput
 
 class VideoTestSrc(IVideoInput):
     name = "Video Test Source"
     
+    # variables
+    live = False
+    pattern = "smpte"
+    
+    # Patterns
+    PATTERNS = ["smpte", "snow", "black", "white", "red", "green", "blue",
+                "circular", "blink", "smpte75", "zone-plate", "gamut",
+                "chroma-zone-plate", "ball", "smpte100", "bar"]
+    
     def get_videoinput_bin(self):
         bin = gst.Bin() # Do not pass a name so that we can load this input more than once.
         
         videosrc = gst.element_factory_make("videotestsrc", "videosrc")
+        videosrc.set_property("pattern", self.pattern)
+        videosrc.set_property("is-live", self.live)
         bin.add(videosrc)
         
         # Setup ghost pad
@@ -44,3 +59,65 @@ class VideoTestSrc(IVideoInput):
         bin.add_pad(ghostpad)
         
         return bin
+    
+    def load_config(self, plugman):
+        self.plugman = plugman
+        
+        try:
+            live = self.plugman.plugmanc.readOptionFromPlugin("Output", self.name, "Live")
+            if live == "True": self.live = True
+            self.pattern = self.plugman.plugmanc.readOptionFromPlugin("Output", self.name, "Pattern")
+        except (ConfigParser.NoSectionError, ConfigParser.NoOptionError):
+            self.plugman.plugmanc.registerOptionFromPlugin("Output", self.name, "Live", self.live)
+            self.plugman.plugmanc.registerOptionFromPlugin("Output", self.name, "Pattern", self.pattern)
+        except TypeError:
+            # Temp fix for issue where reading checkbox the 2nd time causes TypeError.
+            pass
+    
+    def get_widget(self):
+        if self.widget is None:
+            self.widget = QtGui.QWidget()
+            
+            layout = QtGui.QVBoxLayout()
+            self.widget.setLayout(layout)
+            
+            #
+            # Settings
+            #
+            
+            self.liveCheckBox = QtGui.QCheckBox("Live Source")
+            layout.addWidget(self.liveCheckBox)
+            
+            formWidget = QtGui.QWidget()
+            formLayout = QtGui.QFormLayout()
+            formWidget.setLayout(formLayout)
+            layout.addWidget(formWidget)
+            
+            self.patternLabel = QtGui.QLabel("Pattern")
+            self.patternComboBox = QtGui.QComboBox()
+            for i in self.PATTERNS:
+                self.patternComboBox.addItem(i)
+            
+            formLayout.addRow(self.patternLabel, self.patternComboBox)
+            
+            self.widget.connect(self.liveCheckBox, QtCore.SIGNAL('toggled(bool)'), self.set_live)
+            self.widget.connect(self.patternComboBox, QtCore.SIGNAL('currentIndexChanged(const QString&)'), self.set_pattern)
+            
+        return self.widget
+
+    def widget_load_config(self, plugman):
+        self.load_config(plugman)
+        
+        self.liveCheckBox.setChecked(bool(self.live))
+        patternIndex = self.patternComboBox.findText(self.pattern)
+        self.patternComboBox.setCurrentIndex(patternIndex)
+
+    def set_live(self, checked):
+        self.live = checked
+        self.plugman.plugmanc.registerOptionFromPlugin("Output", self.name, "Live", self.live)
+        self.plugman.save()
+        
+    def set_pattern(self, pattern):
+        self.pattern = pattern
+        self.plugman.plugmanc.registerOptionFromPlugin("Output", self.name, "Pattern", self.pattern)
+        self.plugman.save()
