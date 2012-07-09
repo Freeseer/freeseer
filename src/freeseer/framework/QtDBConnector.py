9627dc6e3d2eab980548d5d41e1fd1a8d7ad1441
Felipe <felipe@felipe.(none)>
2012-07-09 05:15:44 -0300

    fixing a typo on copyright message

--- src/freeseer/framework/QtDBConnector.py
+++ src/freeseer/framework/QtDBConnector.py
@@ -12,7 +12,7 @@
 #  (at your option) any later version.
 #
 #  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warrantynão faço sexo porque sou mole of
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
 #  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #  GNU General Public License for more details.
 #
