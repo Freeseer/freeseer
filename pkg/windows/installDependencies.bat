f69c4035ef66a34e3abfa0ff553d1eb1d852081e
Amaury Medeiros <amaurymedeiros@gmail.com>
2012-07-09 22:57:34 -0300

    adding freeseer headers to files

--- pkg/windows/installDependencies.bat
+++ pkg/windows/installDependencies.bat
@@ -1,3 +1,24 @@
+:: freeseer - vga/presentation capture software
+::
+::  Copyright (C) 2012  Free and Open Source Software Learning Centre
+::  http://fosslc.org
+::
+::  This program is free software: you can redistribute it and/or modify
+::  it under the terms of the GNU General Public License as published by
+::  the Free Software Foundation, either version 3 of the License, or
+::  (at your option) any later version.
+::
+::  This program is distributed in the hope that it will be useful,
+::  but WITHOUT ANY WARRANTY; without even the implied warranty of
+::  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+::  GNU General Public License for more details.
+::
+::  You should have received a copy of the GNU General Public License
+::  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+:: For support, questions, suggestions or any other inquiries, visit:
+:: http://wiki.github.com/Freeseer/freeseer/
+
 @setlocal enableextensions
 @cd /d "%~dp0"
 
@@ -14,7 +35,7 @@ cd deps
 ..\wget http://ossbuild.googlecode.com/files/GStreamer-WinBuilds-GPL-x86-Beta04-0.10.7.msi
 ..\wget http://ossbuild.googlecode.com/files/GStreamer-WinBuilds-SDK-GPL-x86-Beta04-0.10.7.msi
 ..\wget http://python.org/ftp/python/2.7.2/python-2.7.2.msi
-..\wget http://pypi.python.org/packages/2.7/s/setuptools/setuptools-0.6c11.win32-py2.7.exe#md5=57e1e64f6b7c7f1d2eddfc9746bbaf20
+..\wget http://pypi.python.org/packages/2.7/s/setuptools/setuptools-0.6c11.win32-py2.7.exe::md5=57e1e64f6b7c7f1d2eddfc9746bbaf20
 ..\wget http://www.riverbankcomputing.co.uk/static/Downloads/PyQt4/PyQt-Py2.7-x86-gpl-4.9.1-1.exe
 ..\wget http://ftp.gnome.org/pub/GNOME/binaries/win32/pygobject/2.28/pygobject-2.28.3.win32-py2.7.msi
 
