750cf93cc1b6eb2d79d2586b2f4eb5ffc4b85b1c
Dennis Ideler <ideler.dennis@gmail.com>
2012-10-01 20:47:13 -0400

    Minor improvements to the CLI
    - Update links
    - Started cleaning the code
    - Get rid of unnecessary imports
    - Add a few comments
    - Exit on CTRL-D
    - Disable auto-repetition of last command on empty line
    - Catch unknown commands the proper way
    - Add help for license and credits (will likely be moved to help.py)

--- src/freeseer/frontend/cli/freeseer_shell.py
+++ src/freeseer/frontend/cli/freeseer_shell.py
@@ -20,38 +20,39 @@
 #  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 # For support, questions, suggestions or any other inquiries, visit:
-# http://wiki.github.com/fosslc/freeseer/
+# http://wiki.github.com/Freeseer/freeseer/
 
-import cmd
-import sys
-import subprocess
-import os
 import logging
 
+from cmd import Cmd
+
+# TODO: Take a look at shlex.split() for tokenization of command arguments.
+
 from freeseer.framework.core import FreeseerCore
-from freeseer_record_parser import FreeSeerRecordParser
-from freeseer_talk_parser import FreeSeerTalkParser
-from freeseer_config_parser import FreeSeerConfigParser
+from freeseer_record_parser import FreeseerRecordParser
+from freeseer_talk_parser import FreeseerTalkParser
+from freeseer_config_parser import FreeseerConfigParser
 from help import Help
 
-class FreeSeerShell(cmd.Cmd):
-    '''
-    Freeseer Shell. Used to provide an interface to the CLI frontend
-    '''
+class FreeseerShell(Cmd):
+    """Freeseer Shell provides an interface to the CLI frontend.
+
+    Note that help methods take precedence to method's doc strings.
+    """
     def __init__(self):        
-        cmd.Cmd.__init__(self)  
+        Cmd.__init__(self)  
         
         self._disable_loggers()       
         self.core = FreeseerCore(self)  
         self.plugman = self.core.get_plugin_manager()
         self.db_connector = self.core.db  
         
-        #Parsers
-        self.config_parser = FreeSeerConfigParser(self.core)
-        self.record_parser = FreeSeerRecordParser(self.core)
-        self.talk_parser = FreeSeerTalkParser(self.core)
+        # Parsers
+        self.config_parser = FreeseerConfigParser(self.core)
+        self.record_parser = FreeseerRecordParser(self.core)
+        self.talk_parser = FreeseerTalkParser(self.core)
         
-        #Auto-complete modes
+        # Auto-complete modes
         self.TALK_MODES = ['show','remove','add','update']
         self.TALK_SHOW_MODES = ['event','events','room','id','all']
         
@@ -62,39 +63,77 @@ class FreeSeerShell(cmd.Cmd):
         self.CONFIG_SET_MODES_EXTENDED = self.CONFIG_SET_MODES + self.PLUGIN_CATEGORIES
         self.CONFIG_SHOW_MODES_EXTENDED = self.CONFIG_SHOW_MODES + self.PLUGIN_CATEGORIES
         
-        self.prompt = "freeseer> "
-        self.intro = "\nfreeseer - video recording and streaming software\n" \
-        "Copyright (C) 2011-2012  Free and Open Source Software Learning Centre\n"       
+        self.TITLE = 'Freeseer'
+        self.VERSION = '3.0.0'
+        self.DESCRIPTION = 'Video recording and streaming software'
+        self.COPYRIGHT = 'Copyright (c) 2011-2012 Free and Open Source Software Learning Centre\n' \
+                         'See http://fosslc.org or http://freeseer.github.com for more information.\n'
+        self.HELP = 'Type "help" for more information.\n'
+        self.intro = '{} {} - {}\n{}\n{}'.format(
+            self.TITLE, self.VERSION, self.DESCRIPTION, self.COPYRIGHT,
+            self.HELP)
+        self.prompt = '?- '
+       
+        # Modify help's documentation strings.
+        #self.doc_header = 'doc_header'
+        #self.misc_header = 'misc_header'
+        #self.undoc_header = 'undoc_header'
+        #ruler = '-'
+
         
     def do_exit(self, line):
-        """
-        Exits the interpreter.
-        """
-        sys.exit()
+        """Exits the interpreter."""
+        return True
         
     def do_quit(self, line):
-        sys.exit()
-        
+        """Exits the interpreter."""
+        return True
+
+    def precmd(self, line):
+        """Exits the interpreter when Ctrl-D pressed.
+         
+        Note that do_EOF() can also be used, but shows up in the help.
+        """
+        if line == "EOF":
+            print  # Ctrl-D is a command without a newline, so print one.
+            return "exit"
+        return line
+
+    def emptyline(self):
+        """Disables auto-repetition of last command when input is an empty line.
+
+        Overrides emptyline's behaviour.
+        """
+        pass
+    
+    def default(self, line):
+        print '{} is not a valid command.'.format(line)
+
+    def help_license(self): # TODO: Better if changed to do_license()?
+        print 'Freeseer is licensed under the GNU GPL version 3.\n' \
+              'See https://raw.github.com/Freeseer/freeseer/master/src/LICENSE\n'
+
+    def help_credits(self): # TODO: Better if changed to do_credits()?
+        print 'Freeseer is maintained by many voluntary contributors.\n' \
+              'The project was started by Andrew Ross and Thanh Ha.\n'
+ 
     def do_record(self, line):        
         if(len(line.split()) == 0):
             print "*** Invalid Syntax"
             return
-        if (line.split()[0] == "help"):
+        elif (line.split()[0] == "help"):
             help_topic = line.replace("help ", "")
             if (help_topic == 'record'):
                 print Help.RECORD_HELP
-            
         else:
             self.record_parser.analyse_command(line)
     
     def help_record(self):
         print Help.RECORD_GENERAL_HELP
 
-
     #TODO   
     def complete_record(self, text, line, start_index, end_index):        
         pass
-            
          
     def do_talk(self, line):        
         if(len(line.split()) == 0):
@@ -114,7 +153,6 @@ class FreeSeerShell(cmd.Cmd):
                 print Help.TALK_UPDATE            
             else:
                 print "Unknow %s topic" % (help_topic)
-            
         else:
             self.talk_parser.analyse_command(line)
     
@@ -142,7 +180,7 @@ class FreeSeerShell(cmd.Cmd):
         if(len(line.split()) == 0):
             print "*** Invalid Syntax"
             return
-        if (line.split()[0] == "help"):
+        elif (line.split()[0] == "help"):
             help_topic = line.replace("help ", "")
             if (help_topic == 'show'):
                 print Help.CONFIG_SHOW
@@ -166,11 +204,9 @@ class FreeSeerShell(cmd.Cmd):
                 print Help.CONFIG_SET
             else:
                 "Unknow %s topic" % (help_topic)
-            
         else:
             self.config_parser.analyse_command(line)
             
-
     def help_config(self):
         print Help.CONFIG_GENERAL_HELP
 
@@ -212,8 +248,6 @@ class FreeSeerShell(cmd.Cmd):
                     property for property in properties
                     if property.upper().startswith(text.upper())
                 ]
-                
-            
         else:
             return self.CONFIG_MODE
         
@@ -221,6 +255,11 @@ class FreeSeerShell(cmd.Cmd):
         self.cmdloop()
         
     def _disable_loggers(self):
+        """ Disables all logging calls of severity INFO and below.
+        
+        The order of logging levels (in increasing severity) is:
+        DEBUG, INFO, WARNING, ERROR, CRITICAL.
+        """
         logging.disable(logging.INFO)
         
     def _get_plugin_name(self, plugin_replaced):
@@ -228,4 +267,3 @@ class FreeSeerShell(cmd.Cmd):
             if entry[0].upper() == plugin_replaced.upper():
                 return entry[1]
         return None
-        
