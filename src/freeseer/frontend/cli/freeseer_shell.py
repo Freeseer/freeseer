88303374e14ebf695e063908e00f60d076757460
Maria91 <me_mcneil@laurentian.ca>
2012-11-05 13:38:21 -0500

    First step in fixing Issue 217. Change help commands to help<command><argument><style>

--- src/freeseer/frontend/cli/freeseer_shell.py
+++ src/freeseer/frontend/cli/freeseer_shell.py
@@ -116,7 +116,37 @@ class FreeseerShell(Cmd):
                 print Help.TALK_SHOW_TALKS
             elif args == 'talk show events':
                 print Help.TALK_SHOW_EVENTS
-            #TODO: and so forth for the rest of the specific help...
+	    elif args == 'talk remove':
+		print Help.Talk_REMOVE
+	    elif args == 'talk add':
+		print Help.TALK_ADD
+	    elif args == 'talk update':
+		print Help.TALK_UPDATE
+	    elif args == 'record':
+		print Help.RECORD
+	    elif args == 'config show':
+		print Help.CONFIG_SHOW
+	    elif args == 'config set audio':
+		print Help.CONFIG_AUDIO_SET
+	    elif args == 'congif set video':
+		print Help.CONFIG_VIDEO_SET
+	    elif args == 'config set video resolution':
+		print Help.CONFIG_VIDEO_RESOLUTION_SET
+	    elif args == 'config set dir':
+		print Help.CONFIG_DIR_SET
+	    elif args == 'config set audio':
+		print Help.CONFIG_SET_AUDIO
+	    elif args == 'config set streaming':
+		print Help.CONFIG_SET_STREAMING
+	    elif args == 'config set file':
+		print Help.CONFIG_SET_FILE
+	    elif args == 'config set':
+		print Help.CONFIG_SET
+	    elif args == 'config':
+		print Help.CONFIG_GENERAL_HELP
+	    else:
+		print Help.TALK_GENERAL_HELP
+            
 
     def help_license(self): # TODO: Better if changed to do_license()?
         print 'Freeseer is licensed under the GNU GPL version 3.\n' \
@@ -179,7 +209,7 @@ class FreeseerShell(Cmd):
         else:
             return self.TALK_MODES
         
-    #TODO          
+    """ #TODO          
     def do_config(self, line):        
         if(len(line.split()) == 0):
             print "*** Invalid Syntax"
@@ -209,8 +239,9 @@ class FreeseerShell(Cmd):
             else:
                 "Unknow %s topic" % (help_topic)
         else:
-            self.config_parser.analyse_command(line)
-            
+            self.config_parser.analyse_command(line) 
+    """ 
+     
     def help_config(self):
         print Help.CONFIG_GENERAL_HELP
 
