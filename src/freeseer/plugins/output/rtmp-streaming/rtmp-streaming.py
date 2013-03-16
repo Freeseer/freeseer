ce1c737cb828ef563ebda811412430df1bd1ec0a
mib <mib@citadel.(none)>
2013-03-16 14:57:23 +0100

    more error handling

--- src/freeseer/plugins/output/rtmp-streaming/rtmp-streaming.py
+++ src/freeseer/plugins/output/rtmp-streaming/rtmp-streaming.py
@@ -485,14 +485,19 @@ class RTMPOutput(IOutput):
         # here is where all the justin.tv streaming presets will be applied
         self.set_stream_url(self.JUSTIN_URL + self.streaming_key)
         self.set_audio_codec('lame')
-        if self.consumer_key and self.consumer_secret:
-            url, self.justin_api = JustinApi.open_request(self.consumer_key, self.consumer_secret)
-            self.justin_api.set_save_method(self.set_justin_api_persistent)
-            self.lineedit_authorization_url.setText(url)
-            self.label_authorization_url.setVisible(True)
-            self.lineedit_authorization_url.setVisible(True)
-            webbrowser.open(url)
-        
+        try:
+            if self.consumer_key and self.consumer_secret:
+                url, self.justin_api = JustinApi.open_request(self.consumer_key, self.consumer_secret)
+                self.justin_api.set_save_method(self.set_justin_api_persistent)
+                self.lineedit_authorization_url.setText(url)
+                self.label_authorization_url.setVisible(True)
+                self.lineedit_authorization_url.setVisible(True)
+                webbrowser.open(url)
+        except KeyError:
+            logging.error("justin.api: authentication failed; supplied credential may be incorrect")
+            self.label_authorization_url.setVisible(False)
+            self.lineedit_authorization_url.setVisible(False)
+            
     def get_properties(self):
         return ['StreamURL', 'AudioQuality', 'VideoBitrate', 'VideoTune', 'AudioCodec', 'Streaming Destination']
     
