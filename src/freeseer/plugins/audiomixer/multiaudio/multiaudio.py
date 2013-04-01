5f8b871c6ec61aa70bd6c7035aef45a68307f892
Aaron Brubacher <aj_brubacher@laurentian.ca>
2013-04-01 18:36:56 -0400

    Update license for multiaudio mixer

--- src/freeseer/plugins/audiomixer/multiaudio/multiaudio.py
+++ src/freeseer/plugins/audiomixer/multiaudio/multiaudio.py
@@ -1,7 +1,7 @@
 '''
 freeseer - vga/presentation capture software
 
-Copyright (C) 2011-2013  Free and Open Source Software Learning Centre
+Copyright (C) 2013  Free and Open Source Software Learning Centre
 http://fosslc.org
 
 This program is free software: you can redistribute it and/or modify
@@ -20,7 +20,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 For support, questions, suggestions or any other inquiries, visit:
 http://wiki.github.com/Freeseer/freeseer/
 
-@author: Thanh Ha
+@author: Aaron Brubacher
 '''
 
 import ConfigParser
