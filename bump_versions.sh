657a8caa025f5064ef8eb54cdc834bd60d413f93
Thanh Ha <thanh.ha@alumni.carleton.ca>
2013-08-17 19:36:17 -0400

    Add tool for bumping version numbers
    This tool is capable of bumping versions of the main freeseer
    app as well as all plugins in it's search path. This should
    satisfy #330.
    
    Fix #330 Freeseer plugins should have same version as Freeseer
    
    Signed-off-by: Thanh Ha <thanh.ha@alumni.carleton.ca>

---
+++ bump_versions.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# freeseer - vga/presentation capture software
+#
+#  Copyright (C) 2011, 2013  Free and Open Source Software Learning Centre
+#  http://fosslc.org
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# For support, questions, suggestions or any other inquiries, visit:
+# http://wiki.github.com/Freeseer/freeseer/
+
+
+# Note: Plugins do not support versions with words in them. The supported
+#       format is numbers with decimals for example 3.0.0
+
+version=$1
+
+# bump the version of Freeseer
+sed -i "s/^__version__ = .*/__version__ = '$version'/" src/freeseer/__init__.py
+
+# search for plugins and bump version
+echo "Bumping plugin versions to $version"
+find src/freeseer/plugins -name "*.freeseer-plugin" -print | xargs sed -i "s/^Version = .*/Version = $version/"
+
