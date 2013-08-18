68f4decc3a11bf48a388d23ae92d800654830d4f
Thanh Ha <thanh.ha@alumni.carleton.ca>
2013-08-17 20:07:45 -0400

    Bump version to 3.0.0-beta4
    Signed-off-by: Thanh Ha <thanh.ha@alumni.carleton.ca>

--- src/freeseer/__init__.py
+++ src/freeseer/__init__.py
@@ -18,7 +18,7 @@ is available on GitHub.
 """
 
 NAME = 'freeseer'
-__version__ = '3.0.0-beta3'
+__version__ = '3.0.0-beta4'
 __author__ = "Free and Open Source Software Learning Center"
 __email__ = "fosslc@gmail.com"
 URL = 'http://github.com/Freeseer/freeseer'
