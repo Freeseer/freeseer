399562a5f4e5b3400a67512fcb2f7d3a75ec225b
Dennis Ideler <ideler.dennis@gmail.com>
2013-08-29 01:41:06 -0400

    Update copyright

--- source/conf.py
+++ source/conf.py
@@ -46,7 +46,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Freeseer'
-copyright = u'2011-2012  Free and Open Source Software Learning Centre'
+copyright = u'© 2013 Free and Open Source Software Learning Centre'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -115,7 +115,7 @@ html_theme = 'sphinxdoc'
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-html_logo = 'freeseer_logo.png' #'../../src/freeseer/frontend/qtcommon/images/freeseer_logo.png'
+html_logo = 'freeseer_logo.png'
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
