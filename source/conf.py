d93d8bf8f9f8d4d413a5107d9c9f3838c76ddc9e
Dennis Ideler <ideler.dennis@gmail.com>
2013-09-16 07:34:44 -0400

    Ignore report page until we find a spot for it

--- source/conf.py
+++ source/conf.py
@@ -70,7 +70,7 @@ release = '3.0.0'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = []
+exclude_patterns = ['contribute/report.rst']
 
 # The reST default role (used for this markup: `text`) to use for all documents.
 #default_role = None
