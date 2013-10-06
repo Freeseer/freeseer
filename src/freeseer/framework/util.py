Freeseer/freeseer

commit f324a101f3fef217682396240f17b7381271ed19

Fix #357 Commandline option to reset Freeseer
Added 3 new options to freeseer-config

--reset                Removes the entire freeseer configuration
                       directory.
--reset-configuration  Removes freeseer.conf and plugins.conf from the
                       configuration directory.
--reset-database       Removes presentations.db from the configuration
                       directory. Resets the talk database.

Signed-off-by: Thanh Ha <thanh.ha@alumni.carleton.ca>

src/freeseer/framework/util.py
@@ -24,6 +24,7 @@
 
 import ctypes
 import os
+import shutil
 import sys
 import unicodedata
 
@@ -136,3 +137,51 @@ def make_shortname(string):
     bad_chars = set("!@#$%^&*()+=|:;{}[]',? <>~`/\\")
     string = "".join(ch for ch in string if ch not in bad_chars)
     return string[0:6].upper()
+
+###
+### Handy functions for reseting Freeseer configuration
+###
+
+def reset(configdir):
+    """Deletes the Freeseer configuration directory"""
+    if validate_configdir(configdir):
+        print 'This will wipe out your freeseer configuration directory.'
+        if confirm_yes() is True:
+            shutil.rmtree(configdir)
+    else:
+        print "%s is not a invalid configuration directory." % configdir
+
+def reset_configuration(configdir):
+    """Deletes the Freeseer configuration files freeseer.conf and plugin.conf"""
+    if validate_configdir(configdir):
+        freeseer_conf = os.path.join(configdir, 'freeseer.conf')
+        plugin_conf = os.path.join(configdir, 'plugin.conf')
+
+        os.remove(freeseer_conf)
+        os.remove(plugin_conf)
+    else:
+        print "%s is not a invalid configuration directory." % configdir
+
+def reset_database(configdir):
+    """Deletes the Freeseer database file"""
+    if validate_configdir(configdir):
+        dbfile = os.path.join(configdir, 'presentations.db')
+        os.remove(dbfile)
+    else:
+        print "%s is not a invalid configuration directory." % configdir
+
+def validate_configdir(configdir):
+    """Validate that the configdir is not one of the blacklisted directories"""
+    if (configdir and configdir != '/' and
+                      configdir != '~' and
+                      configdir != os.path.abspath(os.path.expanduser('~'))):
+        return True
+
+    return False
+
+def confirm_yes():
+    """Prompts the user to confirm by typing 'yes' in response"""
+    confirm = raw_input("Enter 'yes' to confirm: ")
+    if confirm == 'yes':
+        return True
+    return False

src/freeseer-config
@@ -3,7 +3,7 @@
 
 # freeseer - vga/presentation capture software
 #
-#  Copyright (C) 2011  Free and Open Source Software Learning Centre
+#  Copyright (C) 2011, 2013  Free and Open Source Software Learning Centre
 #  http://fosslc.org
 #
 #  This program is free software: you can redistribute it and/or modify
@@ -22,14 +22,37 @@
 # For support, questions, suggestions or any other inquiries, visit:
 # http://wiki.github.com/Freeseer/freeseer/
 
-import signal
+
 import sys
 
-from PyQt4 import QtGui
+if len(sys.argv) > 1:
+    '''If arguments are passed, act as a commandline'''
+    import argparse
+    from freeseer.settings import configdir
+    from freeseer.framework.util import reset
+    from freeseer.framework.util import reset_configuration
+    from freeseer.framework.util import reset_database
 
-from freeseer.frontend.configtool.configtool import ConfigToolApp
+    parser = argparse.ArgumentParser(description='Freeseer Configuration Utility')
+    parser.add_argument("--reset", help="Reset's Freeseer (removes the Freeseer configuration directory)",
+        action="store_true")
+    parser.add_argument("--reset-configuration", help="Reset's Freeseer configuration (removes freeseer.conf and plugins.conf)", action="store_true")
+    parser.add_argument("--reset-database", help="Reset's Freeseer database (removes presentations.db)", action="store_true")
+    args = parser.parse_args()
 
-if __name__ == "__main__":
+    if args.reset:
+        reset(configdir)
+    elif args.reset_configuration:
+        reset_configuration(configdir)
+    elif args.reset_database:
+        reset_database(configdir)
+
+else:
+    '''Launch Freeseer Configuration GUI if no arguments are passed'''
+    import signal
+    from PyQt4 import QtGui
+    from freeseer.frontend.configtool.configtool import ConfigToolApp
+
     signal.signal(signal.SIGINT, signal.SIG_DFL)
     app = QtGui.QApplication(sys.argv)
     main = ConfigToolApp()

commit d43631d3fbd57a303824ef2adbce57d7d15d8e54

Fix build broken by commit f324a101
Fix missing 2 spaces between function names
Add E127 to pep8 ignore list

Signed-off-by: Thanh Ha <thanh.ha@alumni.carleton.ca>

src/freeseer/framework/util.py
@@ -138,9 +138,11 @@ def make_shortname(string):
     string = "".join(ch for ch in string if ch not in bad_chars)
     return string[0:6].upper()
 
+
 ###
 ### Handy functions for reseting Freeseer configuration
 ###
+
 
 def reset(configdir):
     """Deletes the Freeseer configuration directory"""
@@ -150,6 +152,7 @@ def reset(configdir):
             shutil.rmtree(configdir)
     else:
         print "%s is not a invalid configuration directory." % configdir
+
 
 def reset_configuration(configdir):
     """Deletes the Freeseer configuration files freeseer.conf and plugin.conf"""
@@ -162,6 +165,7 @@ def reset_configuration(configdir):
     else:
         print "%s is not a invalid configuration directory." % configdir
 
+
 def reset_database(configdir):
     """Deletes the Freeseer database file"""
     if validate_configdir(configdir):
@@ -169,6 +173,7 @@ def reset_database(configdir):
         os.remove(dbfile)
     else:
         print "%s is not a invalid configuration directory." % configdir
+
 
 def validate_configdir(configdir):
     """Validate that the configdir is not one of the blacklisted directories"""
@@ -178,6 +183,7 @@ def validate_configdir(configdir):
         return True
 
     return False
+
 
 def confirm_yes():
     """Prompts the user to confirm by typing 'yes' in response"""

src/freeseer/tests/__init__.py
@@ -23,7 +23,8 @@
 # http://wiki.github.com/Freeseer/freeseer/
 
 pep8_options = {'max_line_length': 160,
-                'ignore': ['E128',   # Ignore under indents
+                'ignore': ['E127',   # Ignore over indents
+                           'E128',   # Ignore under indents
                            'E221',   # Multiple whitespace before operator
                            'E241']}  # Ignore multiple whitespaces after :
 