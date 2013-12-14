5092e84c4dbaaed1c2dd2969bb95eba3e2af47cb
Thanh Ha <thanh.ha@alumni.carleton.ca>
2013-12-14 18:10:49 -0500

    Rework CLI and GUI into a single startup script
    For developers to launch freeseer from the repo run:
    
        python -m freeseeer [arguments]
    
    Passing no arguments will launch the recording app.
    
    Change-Id: Icf748787a78260703d68dd45836a352fe02bc133
    Signed-off-by: Thanh Ha <thanh.ha@alumni.carleton.ca>

--- src/freeseer/__main__.py
+++ src/freeseer/__main__.py
@@ -3,7 +3,7 @@
 
 # freeseer - vga/presentation capture software
 #
-#  Copyright (C) 2011  Free and Open Source Software Learning Centre
+#  Copyright (C) 2013  Free and Open Source Software Learning Centre
 #  http://fosslc.org
 #
 #  This program is free software: you can redistribute it and/or modify
@@ -22,23 +22,7 @@
 # For support, questions, suggestions or any other inquiries, visit:
 # http://wiki.github.com/Freeseer/freeseer/
 
-import signal
-import sys
+from freeseer.frontend import cli
 
-from PyQt4 import QtGui
-
-from freeseer.frontend.reporteditor.reporteditor import ReportEditorApp
-from freeseer import settings
-
-if __name__ == "__main__":
-    signal.signal(signal.SIGINT, signal.SIG_DFL)
-
-    profile = settings.profile_manager.get()
-    config = profile.get_config('freeseer.conf', settings.FreeseerConfig,
-                                storage_args=['Global'], read_only=True)
-    db = profile.get_database()
-
-    app = QtGui.QApplication(sys.argv)
-    main = ReportEditorApp(config, db)
-    main.show()
-    sys.exit(app.exec_())
+parser = cli.setup_parser()
+cli.parse_args(parser)
