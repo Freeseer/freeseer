5efda28330d77da00bb771c9453e88ab25ff3310
Thanh Ha <thanh.ha@alumni.carleton.ca>
2014-01-08 21:37:36 -0500

    Remove unittests that are no longer valid
    Change-Id: Ied7ba5455a299ad93e908bce7f1b56dc554a30b8

--- src/freeseer/tests/frontend/talkeditor/test_talkeditor.py
+++ src/freeseer/tests/frontend/talkeditor/test_talkeditor.py
@@ -74,109 +74,48 @@ class TestTalkEditorApp(unittest.TestCase):
         '''
         shutil.rmtree(self.profile_manager._base_folder)
 
-        QtTest.QTest.mouseClick(self.talk_editor.editorWidget.closeButton, Qt.Qt.LeftButton)
         del self.app
         del self.talk_editor.app
 
-    def test_add_talk(self):
-        '''
-        Tests a user creating a talk and adding it.
-        '''
+    # def test_add_talk(self):
+    #     '''
+    #     Tests a user creating a talk and adding it.
+    #     '''
 
-        QtTest.QTest.mouseClick(self.talk_editor.editorWidget.addButton, Qt.Qt.LeftButton)
-        self.assertFalse(self.talk_editor.editorWidget.isVisible())
-        self.assertTrue(self.talk_editor.addTalkWidget.isVisible())
+    #     QtTest.QTest.mouseClick(self.talk_editor.editorWidget.addButton, Qt.Qt.LeftButton)
+    #     self.assertFalse(self.talk_editor.editorWidget.isVisible())
+    #     self.assertTrue(self.talk_editor.addTalkWidget.isVisible())
 
-        mTitle = "This is a test"
-        mPresenter = "Me, myself, and I"
-        mEvent = "0 THIS St."
-        mRoom = "Room 13"
+    #     mTitle = "This is a test"
+    #     mPresenter = "Me, myself, and I"
+    #     mEvent = "0 THIS St."
+    #     mRoom = "Room 13"
 
-        # populate talk data (date and time are prepopulated)
-        self.talk_editor.addTalkWidget.titleLineEdit.setText(mTitle)
-        self.talk_editor.addTalkWidget.presenterLineEdit.setText(mPresenter)
-        self.talk_editor.addTalkWidget.eventLineEdit.setText(mEvent)
-        self.talk_editor.addTalkWidget.roomLineEdit.setText(mRoom)
+    #     # populate talk data (date and time are prepopulated)
+    #     self.talk_editor.addTalkWidget.titleLineEdit.setText(mTitle)
+    #     self.talk_editor.addTalkWidget.presenterLineEdit.setText(mPresenter)
+    #     self.talk_editor.addTalkWidget.eventLineEdit.setText(mEvent)
+    #     self.talk_editor.addTalkWidget.roomLineEdit.setText(mRoom)
 
-        # add in the talk
-        QtTest.QTest.mouseClick(self.talk_editor.addTalkWidget.addButton, Qt.Qt.LeftButton)
+    #     # add in the talk
+    #     QtTest.QTest.mouseClick(self.talk_editor.addTalkWidget.addButton, Qt.Qt.LeftButton)
 
-        # find our talk (ensure it was added)
-        found = False
-        row_count = self.talk_editor.editorWidget.editor.model().rowCount() - 1
-        while row_count >= 0 and not found:  # should be at the end, but you never know
-            if self.talk_editor.editorWidget.editor.model().index(row_count, 1).data() == mTitle and \
-                    self.talk_editor.editorWidget.editor.model().index(row_count, 2).data() == mPresenter and \
-                    self.talk_editor.editorWidget.editor.model().index(row_count, 5).data() == mEvent and \
-                    self.talk_editor.editorWidget.editor.model().index(row_count, 6).data() == mRoom:
-                    found = True
-                    # TODO: Select this row
-            row_count -= 1
+    #     # find our talk (ensure it was added)
+    #     found = False
+    #     row_count = self.talk_editor.editorWidget.editor.model().rowCount() - 1
+    #     while row_count >= 0 and not found:  # should be at the end, but you never know
+    #         if self.talk_editor.editorWidget.editor.model().index(row_count, 1).data() == mTitle and \
+    #                 self.talk_editor.editorWidget.editor.model().index(row_count, 2).data() == mPresenter and \
+    #                 self.talk_editor.editorWidget.editor.model().index(row_count, 5).data() == mEvent and \
+    #                 self.talk_editor.editorWidget.editor.model().index(row_count, 6).data() == mRoom:
+    #                 found = True
+    #                 # TODO: Select this row
+    #         row_count -= 1
 
-        self.assertTrue(found, "Couldn't find talk just inserted...")
+    #     self.assertTrue(found, "Couldn't find talk just inserted...")
 
-        # now delete the talk we just created
-        QtTest.QTest.mouseClick(self.talk_editor.editorWidget.removeButton, Qt.Qt.LeftButton)
-
-    def test_add_talk_cancel(self):
-        '''
-        Tests a user creating a talk, but cancelling it instead of adding it.
-        '''
-
-        QtTest.QTest.mouseClick(self.talk_editor.editorWidget.addButton, Qt.Qt.LeftButton)
-        self.assertFalse(self.talk_editor.editorWidget.isVisible())
-        self.assertTrue(self.talk_editor.addTalkWidget.isVisible())
-
-        QtTest.QTest.mouseClick(self.talk_editor.addTalkWidget.cancelButton, Qt.Qt.LeftButton)
-        self.assertFalse(self.talk_editor.addTalkWidget.isVisible())
-        self.assertTrue(self.talk_editor.editorWidget.isVisible())
-
-    def test_close_talkeditor(self):
-        '''
-        Tests the "close" button.
-        Although we close the app using this button after every test (in tearDown()),
-        we are not guaranteed a valid test case in tearDown().
-        '''
-
-        QtTest.QTest.mouseClick(self.talk_editor.editorWidget.closeButton, Qt.Qt.LeftButton)
-        self.assertFalse(self.talk_editor.editorWidget.isVisible())
-
-    def test_clear_all_talks(self):
-        '''
-        Tests the "clear" button on the main window.
-        '''
-
-        if self.talk_editor.editorWidget.editor.model().rowCount() == 0:
-            QtTest.QTest.mouseClick(self.talk_editor.editorWidget.addButton, Qt.Qt.LeftButton)
-            mTitle = "This is a test"
-            mPresenter = "Me, myself, and I"
-            mEvent = "0 THIS St."
-            mRoom = "Room 13"
-            # populate talk data
-            self.talk_editor.addTalkWidget.titleLineEdit.setText(mTitle)
-            self.talk_editor.addTalkWidget.presenterLineEdit.setText(mPresenter)
-            self.talk_editor.addTalkWidget.eventLineEdit.setText(mEvent)
-            self.talk_editor.addTalkWidget.roomLineEdit.setText(mRoom)
-
-            # date and time are prepopulated
-
-            # add in the talk
-            QtTest.QTest.mouseClick(self.talk_editor.addTalkWidget.addButton, Qt.Qt.LeftButton)
-
-        # we need at least 1 talk
-        self.assertTrue(self.talk_editor.editorWidget.editor.model().rowCount() > 0)
-
-        # TODO: ROADBLOCK
-        # Clicking "clear" causes a pop-up confirmation box to appear.
-        # However, there is no link to this popup from inside the talk_editor
-
-        # hit the clear button, hit no. No changes
-        #QtTest.QTest.mouseClick(self.talk_editor.editorWidget.clearButton, Qt.Qt.LeftButton)
-        # TODO: get the pop-up box's focus and click the button
-
-        # hit the clear button, hit yes. Empty DB
-        #QtTest.QTest.mouseClick(self.talk_editor.editorWidget.clearButton, Qt.Qt.LeftButton)
-        # TODO: get the pop-up box's focus and click the button
+    #     # now delete the talk we just created
+    #     QtTest.QTest.mouseClick(self.talk_editor.editorWidget.removeButton, Qt.Qt.LeftButton)
 
     def test_file_menu_quit(self):
         '''
