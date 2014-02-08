f22725cb2c79b4d3a24fbc4631b752bc80ecaad4
Laura Chen <laura.di.chen@gmail.com>
2014-02-08 17:51:05 -0500

    Fix #453 Data and talk title displayed incorrectly in the recording UI
    The Date column from database's presentation table had "Time".
    
    Closes #481

--- src/freeseer/framework/database.py
+++ src/freeseer/framework/database.py
@@ -233,9 +233,9 @@ class QtDBConnector(object):
         # Duplicate time to date field for older RSS / CSV formats
         # If date is empty, and time has a full DateTime, split the DateTime to
         # both Date and Time
+
         if not presentation.date and presentation.time and len(presentation.time) == 16:
             presentation.date, presentation.time = presentation.time[:-6], presentation.time[11:]
-
         QtSql.QSqlQuery(
             '''INSERT INTO presentations VALUES (NULL, "%s", "%s", "%s", "%s", "%s", "%s", "%s", "%s")''' %
             (presentation.title,
@@ -294,7 +294,7 @@ class QtDBConnector(object):
         """Gets the Rooms Model.Useful for Qt GUI based Frontends to load the Model into Views."""
         self.datesModel = QtSql.QSqlQueryModel()
         self.datesModel.setQuery(
-            "SELECT DISTINCT date(Time) FROM presentations WHERE Event='%s' and Room='%s' ORDER BY Date ASC"
+            "SELECT DISTINCT date FROM presentations WHERE Event='%s' and Room='%s' ORDER BY Date ASC"
             % (event, room))
         return self.datesModel
 
