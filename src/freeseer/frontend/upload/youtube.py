ba222ca666c7737d6bd32fe408193056f3657419
Alex Pilon <apilo088@gmail.com>
2014-03-15 16:26:21 -0400

    Fix #528 Youtube API amendments for testing - Changed handle_response definition, does not take tuple anymore
    - Switch from class to module
    
    - Made appropriate changes in cli.py
    
    - Divided up code fore more testability

--- src/freeseer/frontend/upload/youtube.py
+++ src/freeseer/frontend/upload/youtube.py
@@ -30,82 +30,97 @@ from freeseer.framework.youtube import Response
 from freeseer.framework.youtube import YoutubeService
 
 
-class YoutubeFrontend(object):
-    """Frontend class of Youtube Framework
+def get_defaults():
+    """Retrieve the defaults value for client_secrets, video folder, etc.
 
-    Class currently only features command line support
+    Returns:
+        dictionary of default values which are:
+        client_secrets: ~/<configdir>/client_secrets.json
+        oauth2_token: ~/<configdir>/oauth2_token.json
+        video_directory: ~/Videos
     """
+    profile = settings.profile_manager.get("default")
+    config = profile.get_config('freeseer.conf', settings.FreeseerConfig, storage_args=['Global'], read_only=True)
+    return {
+        "video_directory": config.videodir,
+        "oauth2_token": os.path.join(settings.configdir, "oauth2_token.json"),
+        "client_secrets": os.path.join(settings.configdir, "client_secrets.json")
+    }
 
-    @staticmethod
-    def get_defaults():
-        """Retrieve the defaults value for client_secrets, video folder, etc.
 
-        Returns:
-            dictionary of default values which are:
-            client_secrets: ~/<configdir>/client_secrets.json
-            oauth2_token: ~/<configdir>/oauth2_token.json
-            video_directory: ~/Videos
-        """
-        profile = settings.profile_manager.get("default")
-        config = profile.get_config('freeseer.conf', settings.FreeseerConfig, storage_args=['Global'], read_only=True)
-        return {
-            "video_directory": config.videodir,
-            "oauth2_token": os.path.join(settings.configdir, "oauth2_token.json"),
-            "client_secrets": os.path.join(settings.configdir, "client_secrets.json")
-        }
+def handle_response(response_code, response):
+    """Process the response from the Youtube API"""
+    if response_code is Response.SUCCESS:
+        print("The file was successfully uploaded with video id: {}".format(response['id']))
+    elif response_code is Response.UNEXPECTED_FAILURE:
+        print("The file failed to upload with unexpected response: {}".format(response))
+    elif response_code is Response.UNRETRIABLE_ERROR:
+        print("An unretriable HTTP error {} occurred:\n{}".format(response['status'], response['content']))
+    elif response_code is Response.MAX_RETRIES_REACHED:
+        print("The maximum number of retries has been reached")
+    elif response_code is Response.ACCESS_TOKEN_ERROR:
+        print("The access token has expired or been revoked, please run python -m freeseer config youtube")
 
-    def upload(self, files, token, yes):
-        """Uploads a file(s) to YouTube using the YouTube service API
 
-        This function uploads a list of videos and/or directories of videos to YouTube.
+def gather_videos(files):
+    """Gather all valid videos into a set for upload"""
+    # Because we are using a set, no duplicates will be present
+    videos = set()
+    for item in files:
+        # Crawl subfolders
+        if os.path.isdir(item):
+            for root, _, filenames in os.walk(item):
+                for filename in filenames:
+                    filepath = os.path.join(root, filename)
+                    # Check if its a video
+                    if YoutubeService.valid_video_file(filepath):
+                        videos.add(filepath)
+        # If it exists it is a single file, check if its a video
+        elif os.path.exists(item) and YoutubeService.valid_video_file(item):
+            videos.add(item)
+    return videos
 
-        Args:
-            token            - location of an oauth2 token
-            files            - list of files and directories to upload
-            yes              - if True, assume yes to all interaction (default: False)
-        """
-        # check if token exists
-        if not os.path.exists(token):
-            print("{} does not exist, please specify a valid token file".format(token))
-            return
-        # gather videos specified and vids from folders specified into list
-        videos = []
-        for item in files:
-            # crawl subfolders
-            if os.path.isdir(item):
-                for root, dirs, files_ in os.walk(item):
-                    for f in files_:
-                        filepath = os.path.join(root, f)
-                        # check if its a video and make sure its not a duplicate
-                        if YoutubeService.valid_video_file(filepath) and filepath not in videos:
-                            videos.append(filepath)
-            # if it exists it is a single file, check if its a video, and make sure its not a duplicate
-            elif os.path.exists(item) and YoutubeService.valid_video_file(item) and item not in videos:
-                videos.append(item)
-        # now begin upload process
+
+def prompt_user(videos, confirmation=False):
+    """Method to prompt user for confirmation, if yes is specified then no prompt is shown
+
+    Returns: boolean value of final decision
+    """
+    if not confirmation:
+        print("Found videos:")
+        print("\n".join(videos))
+        question = "Are you sure you would like to upload these videos? [Y/n]"
+        confirmation = raw_input(question).lower() in ('', 'y', 'yes')
+    return confirmation
+
+
+def upload(files, token, assume_yes):
+    """Uploads a file(s) to YouTube using the YouTube service API
+
+    This function uploads a list of videos and/or directories of videos to YouTube.
+
+    Args:
+        token            - location of an oauth2 token
+        files            - list of files and directories to upload
+        assume_yes       - if True, assume yes to all interaction (default: False)
+    """
+    # check if token exists
+    if not os.path.exists(token):
+        print("{} does not exist, please specify a valid token file".format(token))
+    else:
+        # Gather videos specified and vids from folders specified into list
+        videos = gather_videos(files)
+        # Now begin upload process
         if not videos:
             print("Nothing to upload")
-        else:
+        # Prompt for confirmation
+        elif prompt_user(videos, confirmation=assume_yes):
             youtube_service = YoutubeService()
-            # authorize with OAuth2 token
+            # Authorize with OAuth2 token
             youtube_service.authorize(token)
-            if not yes:
-                print("Found videos:")
-                print("\n".join(videos))
-            question = "Are you sure you would like to upload these videos? [Y/n] "
-            if yes or raw_input(question).lower() in ('', 'y', 'yes'):
-                for video in videos:
-                    self.handle_response(youtube_service.upload_video(video))
-
-    def handle_response(self, (response_code, response)):
-        """Process the response from the Youtube API"""
-        if response_code is Response.SUCCESS:
-            print("The file was successfully uploaded with video id: {}".format(response['id']))
-        elif response_code is Response.UNEXPECTED_FAILURE:
-            print("The file failed to upload with unexpected response: {}".format(response))
-        elif response_code is Response.UNRETRIABLE_ERROR:
-            print("An unretriable HTTP error {} occurred:\n{}".format(response['status'], response['content']))
-        elif response_code is Response.MAX_RETRIES_REACHED:
-            print("The maximum number of retries has been reached")
-        elif response_code is Response.ACCESS_TOKEN_ERROR:
-            print("The access token has expired or been revoked, please run python -m freeseer config youtube")
+            for video in videos:
+                response_code, response = youtube_service.upload_video(video)
+                handle_response(response_code, response)
+        # Response was no, so do nothing
+        else:
+            print("Exiting...")
