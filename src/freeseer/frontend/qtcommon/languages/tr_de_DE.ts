afdfd804f89183b8969d9f206b2e21a99f0cf765
Faraz Sherwani <farazsherwani@gmail.com>
2014-09-19 10:56:45 -0400

    Fix #540 Improved ConfigTool UI
    - Move directory setting from General to Recording tab
    - Put record to file options before record to stream options
    - Improved resizing of ConfigTool
    - Fixed close button size inconsistency
    - Add Recording tab heading
    - Recording and General tab section headers larger and bold
    - Sub-heading font size determined at runtime
    as larger than regular font size
    - Made widgets in Recording tab fixed size
    - Capitalized the name in AboutWidget
    - Add translations
    - Directory button says "Browse..."
    - Renamed mainlayout to mainLayout for AboutWidget
    - Rename MiscGroupBox and MiscLayout to proper case

--- src/freeseer/frontend/qtcommon/languages/tr_de_DE.ts
+++ src/freeseer/frontend/qtcommon/languages/tr_de_DE.ts
@@ -131,6 +131,11 @@
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../../configtool/configtool.py" line="204"/>
+        <source>Recording</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
         <location filename="../../configtool/configtool.py" line="208"/>
         <source>Audio Input</source>
         <translation>Audioeingang</translation>
@@ -153,6 +158,11 @@
     <message>
         <location filename="../../configtool/configtool.py" line="217"/>
         <source>File Format</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../configtool/configtool.py" line="218"/>
+        <source>Browse</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
